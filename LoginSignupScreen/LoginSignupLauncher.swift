61bf7b9241908d76296d1c5472d08104d7ada5e0
zackaryoconnor <zackary25@gmail.com>
2016-11-30 12:13:12 -0700

    This is the first commit

---
+++ LoginSignupScreen/LoginSignupLauncher.swift
@@ -0,0 +1,9 @@
+//
+//  LoginSignupLauncher.swift
+//  LoginSignupScreen
+//
+//  Created by Zackary O'Connor on 11/30/16.
+//  Copyright © 2016 Zackary O'Connor. All rights reserved.
+//
+
+import Foundation
