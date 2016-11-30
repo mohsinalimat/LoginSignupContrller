66737af899f866e85719752a3fb348e0ec616f64
zackaryoconnor <zackary25@gmail.com>
2016-11-30 13:26:51 -0700

    added a collectin view to the controller

--- LoginSignupScreen/LoginSignupLauncher.swift
+++ LoginSignupScreen/LoginSignupLauncher.swift
@@ -12,6 +12,13 @@ class LoginSignupLauncher: NSObject {
     
     let blackView = UIView()
     
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        return cv
+    }()
+    
     func showLoginSignupController() {
         // show login menu
         
@@ -22,12 +29,23 @@ class LoginSignupLauncher: NSObject {
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
             
             window.addSubview(blackView)
+            window.addSubview(collectionView)
+            
+            let height: CGFloat = 200
+            let y = window.frame.height - height
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
+            
             blackView.frame = window.frame
             blackView.alpha = 0
             
-            UIView.animate(withDuration: 0.5, animations: {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
                 self.blackView.alpha = 1
-            })
+                
+                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+                
+            }, completion: nil)
         }
     }
     
@@ -35,6 +53,11 @@ class LoginSignupLauncher: NSObject {
     func handleDismiss() {
         UIView.animate(withDuration: 0.5) {
             self.blackView.alpha = 0
+            
+            if let window = UIApplication.shared.keyWindow {
+                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }
+            
         }
     }
     
